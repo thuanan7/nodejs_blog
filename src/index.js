02a6f070ec8933af935e8c40e59444d828952581
Nguyen Thuan An <thuanan7@gmail.com>
2023-01-06 20:42:43 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+
+app.get("/trang-chu", (req, res) => {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  return res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
