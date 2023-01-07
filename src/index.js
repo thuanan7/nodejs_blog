f8e41dd55d6f39541567b0671d06d03e5d223eb0
Nguyen Thuan An <thuanan7@gmail.com>
2023-01-07 16:38:04 +0700

    config template engine

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
 app.use(morgan("combined"));
 
-app.get("/trang-chu", (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c = a + b;
-  return res.send("Hello World!");
+// Template Engine
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: "src/resources/views/layouts",
+    partialsDir: "src/resources/views/partials",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app.listen(port, () => {
